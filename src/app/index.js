5f3ccb1927e53c96accd3c076c0fd7062c628eb9
EruliaF (Nisal) <EruliaF (Nisal)>
2017-08-28 21:29:53 +0530

    first commit 2

---
+++ src/app/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: Nisal
+* @Date:   2017-08-28 21:05:03
+* @Last Modified by:   Nisal
+* @Last Modified time: 2017-08-28 21:06:14
+*/
+console.log('test');
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/*
+* @Author: Nisal
+* @Date:   2017-08-28 20:55:13
+* @Last Modified by:   Nisal
+* @Last Modified time: 2017-08-28 20:56:06
+*/
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
